18b130a96e743b8327ec0c2919d217db7037ea9c
Lynn Cyrin <firemagelynn@gmail.com>
2017-11-14 07:10:53 -0800

    add cleanup stuff to build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
+#!/bin/sh
+
 npx node-sass --o css/static scss/ --output-style compressed
+
+# If there are whitespace errors, print the offending file names and fail.
+exec git diff-index --check --cached $against --
