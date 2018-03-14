ad416955994e5f75be468b0d23630cb68b98bfd1
Lisa Marie Maginnis <lisa.marie@hypatiasoftware.org>
2018-03-14 15:41:08 +0000

    Contact Us content and logic

--- js/wufoo.js
+++ js/wufoo.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
     if($(".foli1")) {
+	// Setup checkboxes (hide them)
 	$(".demo-checkbox").hide();
 	$(".newsletter-checkbox").hide();
 
+	// Disable all inputs until user selects who they are:
+	$("#become-partner :input").prop("disabled", true);
+	// Enable first dropdown:
+	$("#foli1 :input").prop("disabled", false);
+	$("#foli2 :input").prop("disabled", false);
+
+	// Show demo on campus partners, hide on all else.
+	// Newsletter is inverse to campus partner.
 	$('select[name="Field1"]').change(function() {
+	    $("#become-partner :input").prop("disabled", false);
 	    if ($(this).val() == "A potential campus partner") {
 		$(".demo-checkbox").show();
 		$(".newsletter-checkbox").hide();
