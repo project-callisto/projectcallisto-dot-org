1512e170a3cf27dfafd0cf9885c93181fc488bae
Gino O <efoddone@gmail.com>
2019-02-15 12:28:42 -0800

    update js

--- js/animate.js
+++ js/animate.js
@@ -31,18 +31,4 @@ $(document).ready(function() {
       });
     }
   });
-
-  const path = window.location.pathname;
-  if (path === '/what-we-do') {
-    $('#wwdNavItem').addClass('active-link');
-  } else if (path === '/who-we-are') {
-    $('#wwaNavItem').addClass('active-link');
-  } else if (path === '/work-with-us') {
-    $('#carNavItem').addClass('active-link');
-  } else if (path === '/contact-us') {
-    $('#contNavItem').addClass('active-link');
-    $('#contNavItemBold').addClass('active-link-bold');
-  } else if (path === '/donate') {
-    $('#donNavItem').addClass('active-link');
-  }
 });
---
+++ js/navProcessing.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('.navbar-toggle').click(function() {
+    $('body').toggleClass('locked');
+  });
+
+  const path = window.location.pathname;
+  if (path === '/what-we-do') {
+    $('#wwdNavItem').addClass('active-link');
+  } else if (path === '/who-we-are') {
+    $('#wwaNavItem').addClass('active-link');
+  } else if (path === '/work-with-us') {
+    $('#carNavItem').addClass('active-link');
+  } else if (path === '/contact-us') {
+    $('#contNavItem').addClass('active-link');
+    $('#contNavItemBold').addClass('active-link-bold');
+  } else if (path === '/donate') {
+    $('#donNavItem').addClass('active-link');
+  }
+});
