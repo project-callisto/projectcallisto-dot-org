1653fdaff55b6d66280fa35f507ab7936da8d63f
Gino O <efoddone@gmail.com>
2019-02-15 10:58:56 -0800

    update active link styling

--- js/animate.js
+++ js/animate.js
@@ -32,8 +32,18 @@ $(document).ready(function() {
     }
   });
 
-  // const path = window.location.pathname;
-  // if (path === '/what-we-do') {
-  //   $('#wwdNavItem').addClass('active-link');
-  // }
+  const path = window.location.pathname;
+  console.log(path);
+  if (path === '/what-we-do') {
+    $('#wwdNavItem').addClass('active-link');
+  } else if (path === '/who-we-are') {
+    $('#wwaNavItem').addClass('active-link');
+  } else if (path === '/work-with-us') {
+    $('#carNavItem').addClass('active-link');
+  } else if (path === '/contact-us') {
+    $('#contNavItem').addClass('active-link');
+    $('#contNavItemBold').addClass('active-link-bold');
+  } else if (path === '/donate') {
+    $('#donNavItem').addClass('active-link');
+  }
 });
