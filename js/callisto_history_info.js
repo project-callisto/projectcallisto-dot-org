ba73d2d74d6435188b78729cdca748a2ef06e7ae
Gino O <efoddone@gmail.com>
2019-03-21 16:39:41 -0700

    CC not Callisto + updated hyperlinks and auto populate years count
    https://app.asana.com/0/920658136814934/1115066326682290

---
+++ js/callisto_history_info.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  var launchYear = 2015;
+  var currentYear = new Date().getFullYear();
+  var yearsActive = currentYear - launchYear;
+
+  var a = ['','one ','two ','three ','four ', 'five ','six ','seven ','eight ','nine ','ten ','eleven ','twelve ','thirteen ','fourteen ','fifteen ','sixteen ','seventeen ','eighteen ','nineteen '];
+  var b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+
+  function inWords (num) {
+      if ((num = num.toString()).length > 9) return 'overflow';
+      n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+      if (!n) return; var str = '';
+      str += (n[3] != 0) ? (a[Number(n[3])] || b[n[3][0]] + ' ' + a[n[3][1]]) + 'thousand ' : '';
+      str += (n[4] != 0) ? (a[Number(n[4])] || b[n[4][0]] + ' ' + a[n[4][1]]) + 'hundred ' : '';
+      str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]): '';
+      return str;
+  }
+
+  document.getElementById('yearCount').textContent = inWords(yearsActive);
+});
